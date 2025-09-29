10693a08d00d49a41e36a79c6cecc57b51d613f7
saiful.islam <saiful.islam@efficientsoftwaresolutions.com>
2025-09-29 20:04:59 +0600

    user

---
+++ src/app/shared/models/company-config.ts
@@ -0,0 +1,15 @@
+export enum EnumStatus {
+  Active = 1,
+  Inactive = 2,
+}
+export enum EnumUser {
+  Active = 1,
+  Inactive = 2,
+  Pending = 0,
+}
+export enum FourCompany {
+  QMS = 0,
+  CHS = 1,
+  OMS = 2,
+  SL = 3,
+}
