79d8becd3f9e647b26429622e090c419d71a5ffd
sahab.uddin <sahab.uddin@efficientsoftwaresolutions.com>
2025-10-29 14:35:23 +0600

    updates some create ticket modal

--- src/app/features/workflow/component/tickets/ticket-center/ticket-centre.ts
+++ src/app/features/workflow/component/tickets/ticket-center/ticket-centre.ts
@@ -33,6 +33,10 @@ export class TicketCentre {
   });
 
   openAddTicket() {
-    this.modalService.show(AddTicketModal, { class: 'modal-lg' });
+    this.modalService.show(AddTicketModal, {
+      backdrop: true,
+      ignoreBackdropClick: true,
+      class: 'modal-xl',
+    });
   }
 }
